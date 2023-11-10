fbb8085ecbc4f1256f0ec3ca176c63f9d9e7a562
kawabataakiko <138648486+kawabataakiko@users.noreply.github.com>
2023-11-10 16:14:07 +0900

    css,js反映

--- sample_02/js/script02.js
+++ sample_02/js/script02.js
@@ -48,3 +48,13 @@
 
 
 
+  const CLASSNAME = "-visible";
+  const TIMEOUT = 1500;
+  const $target = $(".title");
+  
+  setInterval(() => {
+    $target.addClass(CLASSNAME);
+    setTimeout(() => {
+      $target.removeClass(CLASSNAME);
+    }, TIMEOUT);
+  }, TIMEOUT * 2);
